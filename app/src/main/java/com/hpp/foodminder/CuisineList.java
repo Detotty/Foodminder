c7c764a2b6c1decc70f24da963ef8faa57285067
Sharjeelh <sjh@quantumcph.com>
2016-05-09 17:47:25 +0500

    Layout Designs Completion

---
+++ app/src/main/java/com/hpp/foodminder/CuisineList.java
@@ -0,0 +1,19 @@
+package com.hpp.foodminder;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CuisineList extends SlidingDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuisine_list);
+
+    }
+
+}
--- app/src/main/java/com/hpp/foodminder/ViewRestaurant.java
+++ app/src/main/java/com/hpp/foodminder/ViewRestaurant.java
@@ -4,42 +4,31 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class AddItem extends SlidingDrawerActivity {
+public class ViewRestaurant extends SlidingDrawerActivity {
 
-    EditText Notes, name;
-    TextView restName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_view_restaurant);
     }
-
 
     @Override
     protected void onResume() {
         super.onResume();
-        name = (EditText) findViewById(R.id.et_name);
-        restName = (TextView) findViewById(R.id.tv_name);
         ImageView mHome = (ImageView) toolbar.findViewById(R.id.home);
-        mHome.setImageDrawable(getResources().getDrawable(R.drawable.check_mark));
+        mHome.setImageDrawable(getResources().getDrawable(R.drawable.plus));
         TextView mText = (TextView) toolbar.findViewById(R.id.toolbar_title);
         Typeface tf = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");
         mText.setTypeface(tf);
-        restName.setTypeface(tf);
         mHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (!name.getText().toString().isEmpty()) {
 
-                } else {
-                    Toast.makeText(AddItem.this, "Please enter item name", Toast.LENGTH_SHORT).show();
-                }
             }
         });
     }
